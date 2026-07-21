47dff312443c9e6b4df58188aef8ef5f73817252
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-21 21:56:06 +0900

    Initial Commit

---
+++ FCStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FCStreaming
+//
+//  Created by joe on 7/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
