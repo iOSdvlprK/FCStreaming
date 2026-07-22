f4533385631b6ddfde1f6c46cc3e3097db8a89fd
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-22 22:35:23 +0900

    create LoginViewController, configure launch screen, and import assets

---
+++ FCStreaming/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  FCStreaming
+//
+//  Created by joe on 7/21/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.loginButton.layer.cornerRadius = 19
+        self.loginButton.layer.borderColor = UIColor(named: "main-brown")?.cgColor
+        self.loginButton.layer.borderWidth = 1
+    }
+
+    @IBAction func buttonDidTap(_ sender: Any) {
+    }
+}
+
