a138df9ed007e9619fbe417a2755dd8f6cbb76b2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-07-26 23:57:44 +0900

    add header, recommend, and footer sections to home screen
    - introduce HomeHeaderCell, HomeFooterCell, and HomeRecommendContainerCell/ItemCell
    - update HomeSection enum to support the new sections
    - integrate cells and delegate methods in HomeViewController

---
+++ FCStreaming/Home/Cell/HomeFooterCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeFooterCell.swift
+//  FCStreaming
+//
+//  Created by joe on 7/26/26.
+//
+
+import UIKit
+
+class HomeFooterCell: UICollectionReusableView {
+    static let height: CGFloat = 80
+    static let identifier: String = "HomeFooterCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ FCStreaming/Home/Cell/HomeHeaderCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeHeaderCell.swift
+//  FCStreaming
+//
+//  Created by joe on 7/26/26.
+//
+
+import UIKit
+
+class HomeHeaderCell: UICollectionReusableView {
+    static let height: CGFloat = 66
+    static let identifier: String = "HomeHeaderCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
