5f4b503b55827cabec59fe98c36e337fe737f86b
iOSdvlprK <iOSdvlprK@gmail.com>
2026-08-09 00:00:25 +0900

    add MyViewController, setup navigation, and design My Page UI
    - create MyViewController to handle profile configuration and tap actions
    - wrap MyViewController in a Navigation Controller within the Tab Bar
    - add '찜한 채널' (Bookmarked Channels) and '좋아요한 영상' (Liked Videos) placeholder scenes in Main.storyboard
    - configure profile image view with corner radius and bind tap actions

---
+++ FCStreaming/MY/MyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MyViewController.swift
+//  FCStreaming
+//
+//  Created by joe on 8/8/26.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.profileImageView.layer.cornerRadius = 5
+    }
+
+    @IBAction func bookmarkDidTap(_ sender: Any) {
+        self.performSegue(withIdentifier: "bookmark", sender: nil)
+    }
+    
+    @IBAction func favoriteDidTap(_ sender: Any) {
+        self.performSegue(withIdentifier: "favorite", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
